26469922e8f3492bf68a0fc92d94403fd1e8cff7
NajeebFlutterDev <najeebullah@vorixasolutions.com>
2026-07-08 15:42:54 +0500

    complete python day 1 basics

---
+++ Phase-0-Python/day01/day01.py
@@ -0,0 +1,38 @@
+# --------------------------------------------------------
+# Defining variables of different data types
+name = 'Najeeb'
+age = 25
+cgpa = 3.64
+is_flutter_dev = True
+# --------------------------------------------------------
+
+print("--------------------------------------------------------")
+# Printing the values of the variables
+print("My name is", name)
+print("My age is", age)
+print("My CGPA is", cgpa)
+print("Am I a Flutter developer?", is_flutter_dev)
+# --------------------------------------------------------
+
+print("--------------------------------------------------------")
+# Printing the data types of the variables
+print("The data type of name is:", type(name))
+print("The data type of age is:", type(age))
+print("The data type of cgpa is:", type(cgpa))
+print("The data type of is_flutter_dev is:", type(is_flutter_dev))
+# --------------------------------------------------------
+
+
+print("--------------------------------------------------------")
+# Taking input from the user
+input_name = input("Enter your name: ")
+input_age = int(input("Enter your age: "))
+input_university = input("Enter your University Name: ")
+# --------------------------------------------------------
+
+print("--------------------------------------------------------")
+# Printing the values of the user input
+print("Welcome", input_name)
+print("Age :", input_age)
+print("University :", input_university)
+# --------------------------------------------------------
---
+++ Phase-0-Python/day01/task.py
@@ -0,0 +1,10 @@
+print("--------------------------------------------------------")
+first_number = int(input("Enter the first number: "))
+second_number = int(input("Enter the second number: "))
+
+print("--------------------------------------------------------")
+print("The sum of", first_number, "and", second_number, "is:", first_number + second_number)
+print("The difference of", first_number, "and", second_number, "is:", first_number - second_number)
+print("The product of", first_number, "and", second_number, "is:", first_number * second_number)
+if second_number != 0:
+    print("The division of", first_number, "by", second_number, "is:", first_number / second_number)
